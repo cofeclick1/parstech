635cc59f6d4a23f1e4834d82a874b7fd7c2060c6
akradimo <abasabadimos@gmail.com>
2025-05-23 02:44:09 +0400

    سایدبرا با منوی کاربر و پاپ اپ کاربر

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -389,6 +389,9 @@
         background: #222d32;
     }
 </style>
+<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js"></script>
+
 <script>
     // فعالسازی آکاردئون واقعی با بستن منوی قبلی
     document.addEventListener("DOMContentLoaded", function () {
