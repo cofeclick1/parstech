25b8ea06b8211500bc292d57cb09eaff82603e43
akradimo <abasabadimos@gmail.com>
2025-05-23 04:30:57 +0400

    Update 2025_05_23_001839_create_accounts_table.php

--- database/migrations/2025_05_23_001839_create_accounts_table.php
+++ database/migrations/2025_05_23_001839_create_accounts_table.php
@@ -4,9 +4,9 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateAccountsTable extends Migration
+return new class extends Migration
 {
-    public function up()
+    public function up(): void
     {
         Schema::create('accounts', function (Blueprint $table) {
             $table->id();
@@ -21,8 +21,8 @@ class CreateAccountsTable extends Migration
         });
     }
 
-    public function down()
+    public function down(): void
     {
         Schema::dropIfExists('accounts');
     }
-}
+};
